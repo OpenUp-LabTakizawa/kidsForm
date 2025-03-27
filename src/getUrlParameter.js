f5cb736bcb7b17115ee139bc4e65399b4da49dc0
tarasum1 <t.tarasumi0712@gmail.com>
2025-03-27 11:27:32 +0900

    URLパラメータ渡しでフォーム初期入力機能を追加

---
+++ src/getUrlParameter.js
@@ -0,0 +1,13 @@
+/*
+URLパラメータ値渡し
+https://qiita.com/hiroyukiwk/items/5162a8861f716bcd5fd7
+*/
+
+const urlParams = new URLSearchParams(window.location.search);
+const param_Id = urlParams.get('id'); // パラメータの値を取得
+const param_DisplayName = urlParams.get('displayName'); // パラメータの値を取得
+
+// 初期入力
+document.getElementById("line_userid").value = param_Id;
+document.getElementById("line_displayName").value = param_DisplayName;
+
